2f7ea5e66fde226bfa6637a1c6b23fdd9df43ab9
svalverde007 <servio.valverde.uemv@gmail.com>
2025-12-18 17:05:47 -0500

    problema 05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -5,10 +5,43 @@
  */
 package paquete5;
 
-/**
- *
- * @author reroes
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+Dado el siguiente arreglo
+
+String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+ {"Carrie Burton", "Lauren Rice"},
+{"Emma Escobar", "Lori Flores"},
+{"Steven West", "Toni Martin"}
+ };
+Imprima en pantalla a través de una variable acumuladora solo los estudiantes que tienen como primera letra S,P,T
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String cadena = "";
+
+        System.out.println("--- Estudiantes con iniciales S,P,T  ---\n");
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                String nombre = estudiantes[i][j];
+                char letra = nombre.charAt(0);
+                if (letra == 'S' || letra == 'P' || letra == 'T') {
+                    cadena += nombre + ".\n";
+                }
+            }
+        }
+        System.out.println(cadena);
+    }
 }
